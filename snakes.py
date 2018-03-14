678b6b7b4aacc8f03aa2ed4d8e00bd3c1b99b7c7
Guillem Pascual I Guinovart <guillem.pascual@ub052229.mat.ub.edu>
2018-03-14 12:41:40 +0100

    Comit

--- snakes.py
+++ snakes.py
@@ -1,6 +1,7 @@
 # SNAKES GAME
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
 
+import os
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
